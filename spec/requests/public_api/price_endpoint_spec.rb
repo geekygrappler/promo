c3362a413ea0f79a69ecf32eae67ffccd499797b
Andy Brown <andy@citypantry.com>
2017-06-04 22:57:29 +0100

    WIP tests added

--- spec/requests/public_api/price_endpoint_spec.rb
+++ spec/requests/public_api/price_endpoint_spec.rb
@@ -22,153 +22,165 @@ describe 'Price endpoint:', type: :request do
     )
   end
 
-  describe 'SpecificCustomer promotions' do
-    before(:each) do
-      @promotion.add_constraint SpecificCustomerConstraint.new
-      @promotion.save
+  describe 'Constraints:' do
+    describe 'SpecificCustomer promotions' do
+      before(:each) do
+        @promotion.add_constraint SpecificCustomerConstraint.new
+        @promotion.save
 
-      Promocode.create(
-        code: code,
-        customer_email: 'hodder@winterfell.com',
-        promotion_id: @promotion.id
-      )
-    end
-    it 'should return a price when the correct customer email is passed' do
-      params = {
-        data: {
-          type: 'promocodes',
-          attributes: {
-            code: 'xyz123',
-            'customer-email': 'hodder@winterfell.com'
-          }
-        },
-        included: {
-          type: 'carts',
-          attributes: {
-            'item-total': 27,
-            'delivery-total': 7
-          }
-        }
-      }
-
-      get '/api/v1/price', params: params, headers: authorization_header
-
-      expect(response).to have_http_status(200)
-
-      expect(json_api_attributes['total'].to_i).to eq(34)
-      expect(json_api_attributes['item-total'].to_i).to eq(27)
-      expect(json_api_attributes['delivery-total'].to_i).to eq(7)
-    end
-
-    it 'should return an error when the wrong customer email is passed' do
-      params = {
-        data: {
-          type: 'promocodes',
-          attributes: {
-            code: code,
-            'customer-email': 'theon@pyke.com'
-          }
-        },
-        included: {
-          type: 'carts',
-          attributes: {
-            'total': 39
+        Promocode.create(
+          code: code,
+          customer_email: 'hodder@winterfell.com',
+          promotion_id: @promotion.id
+        )
+      end
+      it 'should return a price when the correct customer email is passed' do
+        params = {
+          data: {
+            type: 'promocodes',
+            attributes: {
+              code: 'xyz123',
+              'customer-email': 'hodder@winterfell.com'
+            }
+          },
+          included: {
+            type: 'carts',
+            attributes: {
+              'item-total': 27,
+              'delivery-total': 7
+            }
           }
         }
-      }
 
-      get '/api/v1/price', params: params, headers: authorization_header
+        get '/api/v1/price', params: params, headers: authorization_header
 
-      expect(response).to have_http_status(422)
+        expect(response).to have_http_status(200)
 
-      expect(json['errors'][0]['title']).to match('This promocode doens\'t belong to this customer')
-    end
+        expect(json_api_attributes['total'].to_i).to eq(34)
+        expect(json_api_attributes['item-total'].to_i).to eq(27)
+        expect(json_api_attributes['delivery-total'].to_i).to eq(7)
+      end
 
-    it 'should return an error when no customer email is passed' do
-      params = {
-        data: {
-          type: 'promocodes',
-          attributes: {
-            code: code
-          }
-        },
-        included: {
-          type: 'carts',
-          attributes: {
-            'total': 39
-          }
-        }
-      }
-
-      get '/api/v1/price', params: params, headers: authorization_header
-
-      expect(response).to have_http_status(422)
-
-      expect(json['errors'][0]['title']).to match('This promotion requires a customer email address')
-
-    end
-  end
-
-  describe 'Promotion period constraints on promotions' do
-    before(:each) do
-      Promocode.create(
-        code: code,
-        promotion_id: @promotion.id
-      )
-    end
-    it 'should prevent a promocode being used after the promotion has ended' do
-      @promotion.start_date = (DateTime.now - 20).utc.iso8601
-      @promotion.end_date = (DateTime.now - 2).utc.iso8601
-      @promotion.save
-
-      params = {
-        data: {
-          type: 'promocodes',
-          attributes: {
-            code: code
-          }
-        },
-        included: {
-          type: 'carts',
-          attributes: {
-            'item-total': 27,
-            'delivery-total': 7
+      it 'should return an error when the wrong customer email is passed' do
+        params = {
+          data: {
+            type: 'promocodes',
+            attributes: {
+              code: code,
+              'customer-email': 'theon@pyke.com'
+            }
+          },
+          included: {
+            type: 'carts',
+            attributes: {
+              'total': 39
+            }
           }
         }
-      }
 
-      get '/api/v1/price', params: params, headers: authorization_header
+        get '/api/v1/price', params: params, headers: authorization_header
 
-      expect(response).to have_http_status(422)
+        expect(response).to have_http_status(422)
 
-      expect(json['errors'][0]['title']).to eq('This promotion has ended')
-    end
+        expect(json['errors'][0]['title']).to match('This promocode doens\'t belong to this customer')
+      end
 
-    it 'should prevent a promocode being used before it has started' do
-      @promotion.start_date = (DateTime.now + 1).utc.iso8601
-      @promotion.save
-
-      params = {
-        data: {
-          type: 'promocodes',
-          attributes: {
-            code: code
-          }
-        },
-        included: {
-          type: 'carts',
-          attributes: {
-            'item-total': 27,
-            'delivery-total': 7
+      it 'should return an error when no customer email is passed' do
+        params = {
+          data: {
+            type: 'promocodes',
+            attributes: {
+              code: code
+            }
+          },
+          included: {
+            type: 'carts',
+            attributes: {
+              'total': 39
+            }
           }
         }
-      }
 
-      get '/api/v1/price', params: params, headers: authorization_header
+        get '/api/v1/price', params: params, headers: authorization_header
 
-      expect(response).to have_http_status(422)
+        expect(response).to have_http_status(422)
 
-      expect(json['errors'][0]['title']).to eq("This promotion has not started, it starts on #{@promotion.start_date.to_s}")
+        expect(json['errors'][0]['title']).to match('This promotion requires a customer email address')
+
+      end
+    end
+
+    describe 'Promotion period constraints on promotions' do
+      before(:each) do
+        Promocode.create(
+          code: code,
+          promotion_id: @promotion.id
+        )
+      end
+      it 'should prevent a promocode being used after the promotion has ended' do
+        @promotion.start_date = (DateTime.now - 20).utc.iso8601
+        @promotion.end_date = (DateTime.now - 2).utc.iso8601
+        @promotion.save
+
+        params = {
+          data: {
+            type: 'promocodes',
+            attributes: {
+              code: code
+            }
+          },
+          included: {
+            type: 'carts',
+            attributes: {
+              'item-total': 27,
+              'delivery-total': 7
+            }
+          }
+        }
+
+        get '/api/v1/price', params: params, headers: authorization_header
+
+        expect(response).to have_http_status(422)
+
+        expect(json['errors'][0]['title']).to eq('This promotion has ended')
+      end
+
+      it 'should prevent a promocode being used before it has started' do
+        @promotion.start_date = (DateTime.now + 1).utc.iso8601
+        @promotion.save
+
+        params = {
+          data: {
+            type: 'promocodes',
+            attributes: {
+              code: code
+            }
+          },
+          included: {
+            type: 'carts',
+            attributes: {
+              'item-total': 27,
+              'delivery-total': 7
+            }
+          }
+        }
+
+        get '/api/v1/price', params: params, headers: authorization_header
+
+        expect(response).to have_http_status(422)
+
+        expect(json['errors'][0]['title']).to eq("This promotion has not started, it starts on #{@promotion.start_date.to_s}")
+      end
+    end
+
+    describe 'MinimumBasketTotal promotions' do
+      before(:each) do
+        @promotion.add_constraint('MinimumBasketTotal')
+        @promotion.save
+      end
+      it 'should price a cart that equals or exceeds the minimum basket total' do
+        
+      end
     end
   end
 end
