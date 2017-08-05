a213d9098fd9efb4eb6c9de88a5c643cf25bd6fc
Andy Brown <brownie3003@gmail.com>
2017-08-05 17:02:41 +0200

    chore: add surge to CSRF

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Promo
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:4200'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
