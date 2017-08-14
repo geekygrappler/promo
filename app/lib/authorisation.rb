c790735f2f716138a446a5df335481c61b8481d5
Andy <brownie3003@gmail.com>
2017-08-14 17:25:03 +0200

    hack: allow web-client to be authorised on public api.

--- app/lib/authorisation.rb
+++ app/lib/authorisation.rb
@@ -1,5 +1,10 @@
 module Authorisation
   def set_user_from_access_token
+    # TODO this is nasty, if we're hitting public API from our own web client, we won't have
+    # an API key, we'll have devise token belongs to the current user
+    if current_user
+      @user = current_user and return
+    end
     api_key = ApiKey.find_by(access_token: request.headers['Authorization'])
 
     if api_key.nil?
