ee30cae6f1b7e848408d5af2c198b2b3f70a5b7a
Andy <brownie3003@gmail.com>
2017-08-27 12:40:20 +0200

    fix: add minimum basket total to serialized response

--- app/serializers/promotion_serializer.rb
+++ app/serializers/promotion_serializer.rb
@@ -1,5 +1,5 @@
 class PromotionSerializer < ActiveModel::Serializer
   attributes :name, :start_date, :end_date, :modifiers, :constraints, :items_percentage_discount,
              :delivery_percentage_discount, :total_percentage_discount, :items_absolute_discount,
-             :delivery_absolute_discount, :total_absolute_discount
+             :delivery_absolute_discount, :total_absolute_discount, :minimum_basket_total
 end
